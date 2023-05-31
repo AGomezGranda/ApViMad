ff97769f23df10ba69fe25332af7393b60d88845
AGomezGranda <a.gomez191@usp.ceu.es>
2023-05-31 16:47:46 +0200

    revertir cambios de mongodb

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// const axios = require('axios');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,28 +8,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var alquilarRouter = require('./routes/alquilar');
-
 var polideportivoRouter = require('./routes/polideportivo');
 var colegioRouter = require('./routes/colegio')
-var comprarRouter = require('./routes/comprar');
-
 var app = express();
-
-//Conexion a la base de datos, MongoDB
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-const mongoURI = 'mongodb+srv://agomez:Apolin17@cluster-alvarog.vfwblbm.mongodb.net/Cluster-AlvaroG';
-
-MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to MongoDB Atlas');
-    const db = client.db('sample_airbnb');
-    const collection = db.collection('listingsAndReviews');
-    app.locals.collection = collection;
-
-  })
-  .catch(error => console.error(error));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,15 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/alquilar', alquilarRouter);
-
 app.get('/polideportivo', polideportivoRouter);
 app.get('/colegio', colegioRouter)
 
-
-app.get('/comprar', comprarRouter);
-
-// app.use('/alquilar', alquilarRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/comprar.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/comprar', function(req, res, next) {
-  res.render('comprar', { title: 'ApViMad' });
-});
-
-module.exports = router;
